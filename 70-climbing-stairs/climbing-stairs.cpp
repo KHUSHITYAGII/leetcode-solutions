f36ea48ca56f14e727ab3e8bed0fcc5ef2b745b1
Khushi Tyagi <tyagikhushi161@gmail.com>
2026-09-15 08:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (73.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     int t[46];
     int solve(int n){
+        if(n == 0) return 1;
         if(n == 1) return 1;
-        if(n == 2) return 2;
 
         if(t[n] != -1){
             return t[n];
         }
-        int one_step = solve(n-1);
-        int two_step = solve(n-2);
+        int step_one = solve(n-1);
+        int step_two = solve(n-2);
 
-        return t[n]  = one_step + two_step;
+        return t[n] = step_one + step_two;
+
     }
     int climbStairs(int n) {
         memset(t,-1,sizeof(t));
-        return solve(n);   
+        return solve(n);
     }
 };
